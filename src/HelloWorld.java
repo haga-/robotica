f591934da5b0339109bb2320b76ba9796a03aa97
haga- <heitor.haga@gmail.com>
2017-11-21 23:16:43 -0200

    Código para contar transições de cores pelo sensor de cor e código para pegar a distância pelo sensor ultrassonico

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -16,11 +16,13 @@ public class HelloWorld {
         UltrasonicSensor us = new UltrasonicSensor(SensorPort.S4);
         NXTRegulatedMotor motorB = Motor.B;
         NXTRegulatedMotor motorC = Motor.C;
+        /* Odômetro */
+        /*
         int curr_color, last_color, transitions = 0;
         double distance = 0;
         curr_color = last_color = cs.getColor().getColor();
         
-        while (transitions < 18) {
+        while (distance < 100) {
         	motorB.forward();
         	motorC.forward();
         	// Branco 0,5cm; Preto 1,5cm
@@ -30,11 +32,20 @@ public class HelloWorld {
         		distance += curr_color == Color.WHITE ?  1.5 : 0.5; 
         		last_color = curr_color;
         	}
-        	
-        	System.out.println("UltraSonic distance: " + us.getDistance());
-        	System.out.println("Distância: " + distance);
+        	System.out.println("Transições: " + transitions);
             
+        }*/
+        
+        /* Sensor ultrassonico */
+        int distance = us.getDistance(); 
+        while(distance > 100) {
+        	motorB.forward();
+        	motorC.forward();
+        	distance = us.getDistance();
+        	System.out.println(distance);
         }
+        motorB.stop(true);
+        motorC.stop();
         
         Button.waitForAnyPress();
         
